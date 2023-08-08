47fd4e15fe279429116cead8b14fcbb7a97db07d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-08 10:41:08 +0200

    task

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,61 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+    /*Написать метод, который принимает 3 числа int (day, month, year)
+     возвращает строку вида "08. August 2023 "
+     */
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int day = 8;
+        int month = 8;
+        int year = 2023;
+        System.out.println(dateToString(day, month, year));
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
+
+    public static String dateToString(int day, int month, int year) {
+        String res = "";
+        String strMonth = "";
+        switch (month) {
+            case 1:
+                strMonth = "January";
+                break;
+            case 2:
+                strMonth = "February";
+                break;
+            case 3:
+                strMonth = "March";
+                break;
+            case 4:
+                strMonth = "April";
+                break;
+            case 5:
+                strMonth = "May";
+                break;
+            case 6:
+                strMonth = "June";
+                break;
+            case 7:
+                strMonth = "July";
+                break;
+            case 8:
+                strMonth = "August";
+                break;
+            case 9:
+                strMonth = "September";
+                break;
+            case 10:
+                strMonth = "October";
+                break;
+            case 11:
+                strMonth = "November";
+                break;
+            case 12:
+                strMonth = "December";
+                break;
+        }
+        res = (day<10?"0":"") + day +". " + strMonth +" " + year;
+        return res;
+
+    }
+
 }
